68ac4584455dc671e28ac4cb42f06912d1fa1578
demarkiusmedley <greenlordsinc@gmail.com>
2018-09-30 16:41:25 -0500

    Practice work wee 1

--- evaluated_expressions.rb
+++ evaluated_expressions.rb
@@ -1,3 +1,21 @@
-puts 3 + 3 + 3 * 10== 36
-puts (2*4)/2-4==0
-puts
+puts 3 + 3 + 3 * 10 == 36
+puts (2*4)/2-4 == 0
+puts "hello" * 3 == "hello" * 3
+puts "hello" + "3" == "hello" + "3"
+puts !true == !!!true
+puts false == !true
+puts true && !false
+puts false || true 
+puts false || true && 3 == 3
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ untitled.rb
@@ -0,0 +1,5 @@
+def check_equality(value_1, value_2)
+puts value_1
+end
+
+check_equality(1, 2)
